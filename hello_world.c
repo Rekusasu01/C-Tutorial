550269a07f8b2203ed948c816d7ebeeed7e664dc
L.Venus <92352091+Rekusasu01@users.noreply.github.com>
2024-07-15 22:04:58 +0800

    WWN
    Working with numbers

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main() {
-printf  ("Hello World!\n");
+
+    // Declare variables
+    int  age = 24;
+    float gpa = 3.5;
+    char grade = 'A';
+    char school[] = "AMA PACO";
+    
+    printf("Male:\n"
+            "Age:%d\n"
+            "Gpa:%.2f\n"
+            "Grade:%c\n"
+            "School:%s\n" ,age ,gpa ,grade, school);
+
+    float Fav = 123.456;
+    printf("My Favorite %s is %.3f","number" ,Fav);
+
+    return 0;
 }
